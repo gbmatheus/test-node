6cd9f8686b610588ab195c093d2e4490a70fc5b8
gbmatheus <gabrielmatheus2503@gmail.com>
2021-01-18 01:13:47 -0300

    Adicionando createArrayRandom

---
+++ src/q5/createArrayRandom.js
@@ -0,0 +1,17 @@
+function createArrayRandom (length) {
+  const arrayRandom  = []
+
+  for (let index = 0; index < length; index++) {
+    numberRandom = Math.random();
+    
+    // numberRandom = Math.random() * 10;
+    // numberRandom = Math.round(numberRandom);
+
+    arrayRandom.push(numberRandom)
+  }
+
+  return arrayRandom;
+  
+}
+
+module.exports = createArrayRandom;
