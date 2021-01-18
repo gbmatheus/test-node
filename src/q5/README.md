06b9dd607d1a2c01852c6f4fa567084d8d3e38bf
gbmatheus <gabrielmatheus2503@gmail.com>
2021-01-18 01:15:22 -0300

    Resolução questão 5

---
+++ src/q5/README.md
@@ -0,0 +1,5 @@
+# Questão 5 - Q5
+
+Use a função criada anteriormente, crie um array de valores para o delay de
+forma aleatória e chame a função usando a biblioteca async. Imprima cada resultado
+em tela.
---
+++ src/q5/pingPong.test.js
@@ -0,0 +1,39 @@
+const ping = require("./pingPong");
+const pingArray = require('./pingArray');
+const createArrayRandom = require('./createArrayRandom');
+
+describe("Function Async Ping", () => {
+  // Melhorar teste com tratamento de erro
+  test("It should error for a delay bigger than 3", async () => {
+    const err = await ping(4);
+
+    // expect(err).toEqual('Delay bigger than 3');
+    expect(err).toEqual('error');
+  });
+
+  test("It should return pong immediately, no argument passed", async () => {
+    const pong = await ping();
+
+    expect(pong).toEqual("pong");
+  });
+
+  test("It should return pong after 3 seconds", async () => {
+    const pongAfter3s = await ping(3);
+
+    expect(pongAfter3s).toEqual("pong");
+  });
+
+  test("It should return a array number random", async () => {
+    const resultArrayRandom = createArrayRandom(5);
+
+    expect(resultArrayRandom.length).toBe(5);
+  });
+
+  test("It should return [pong, error, pong] for array [1,4,2]", async () => {
+    const resultPing = await pingArray([1,4,2]);
+
+    expect(resultPing).toEqual(['pong', 'error', 'pong']);
+  });
+
+
+});
