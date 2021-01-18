b3a93effcbf2e92c06721e58fa01767c3a0675f9
gbmatheus <gabrielmatheus2503@gmail.com>
2021-01-18 01:15:55 -0300

    Alterando promise e alterando retorno de error

--- src/q4/pingPong.js
+++ src/q4/pingPong.js
@@ -1,26 +1,22 @@
-async function ping(delay) {
-  try {
-
-    if(delay === undefined) {
-      return 'pong';
-    }
-
-    if (delay > 3) {
-      throw new Error("Delay bigger than 3");
-    }
-
-    // let promiseTimeout = new Promise(resolve => setTimeout(() => {resolve('pong')}, delay * 1000));
-    let resultPromiseDelay = await new Promise(resolve => 
-      setTimeout(() => {
-        resolve('pong')
-      
-      }, delay * 1000));
-    
-    return resultPromiseDelay;
-
-  } catch (error) {
-    return error;
+function ping(delay) {
+  if (delay === undefined) {
+    return "pong";
   }
+
+  if (delay > 3) {
+    return "error";
+  }
+
+  // let promiseTimeout = new Promise(resolve => setTimeout(() => {resolve('pong')}, delay * 1000));
+  const resultPromiseTimeout = new Promise((resolve) =>
+    setTimeout(() => {
+      resolve("pong");
+    }, delay * 1000)
+  )
+    .then((result) => result)
+    .catch((err) => {});
+
+  return resultPromiseTimeout;
 }
 
 module.exports = ping;
